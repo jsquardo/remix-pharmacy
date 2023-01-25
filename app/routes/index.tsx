c0aefbeb68a60c988ce824c9c1973020c3f74a42
Johnny Squardo <johnnysquardo@gmail.com>
2023-01-25 17:29:24 -0500

    Added utils and prisma

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,4 @@
+import type { MetaFunction } from "@remix-run/node";
 import type { LinksFunction } from "@remix-run/node";
 
 import stylesUrl from "~/styles/index.css";
@@ -5,3 +6,12 @@ import stylesUrl from "~/styles/index.css";
 export const links: LinksFunction = () => {
 	return [{ rel: "stylesheet", href: stylesUrl }];
 };
+
+export const meta: MetaFunction = () => ({
+	title: "HowardCenterPharmacy",
+	description: "The Howard Center Pharmacy in Tifton, Georgia!",
+});
+
+export default function IndexRoute() {
+	return <div>Hello Index Route</div>;
+}
